8a4b01d911cfc3881b1eb5a9f4ec9dac5c7c6634
PULAK-0308 <jainpulak0308@gmail.com>
2024-12-30 15:17:02 +0530

    middleware and authentication added

--- models/Person.js
+++ models/Person.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-
+const bcrypt=require('bcrypt');
 //define person schema
 const personSchema=new mongoose.Schema({
     name:{
@@ -29,8 +29,50 @@ const personSchema=new mongoose.Schema({
     salary:{
         type:Number,
         required:true
+    },
+    username:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
     }
 })
+
+
+personSchema.pre('save',async function(next){
+    const person=this;
+
+    //hash the password only if it has been modified (or is new)
+    if(!person.isModified('password'))
+        return next();
+    try{
+        //hash password gneration
+        const salt=await bcrypt.genSalt(10);
+
+        //hash password
+        const hashedPassword=await bcrypt.hash(person.password,salt);
+        console.log(hashedPassword)
+        person.password=hashedPassword;
+        console.log(this.password)
+        console.log(person.password)
+        next();
+    }catch(err){
+        return next(err);
+    }
+})
+
+personSchema.methods.comparePassword=async function(candidatePassword){
+    try{
+        console.log(this.password)
+        const isMatch=await bcrypt.compare(candidatePassword,this.password);
+        return isMatch;
+    }catch(err){
+        throw err;
+    }
+}
+
 
 //create Person model
 const Person = mongoose.model('Person', personSchema);
--- server.js
+++ server.js
@@ -2,16 +2,29 @@ const express = require('express')
 const app = express()
 const db=require('./db')
 require("dotenv").config();
+const passport=require('./auth');
+
 const bodyParse=require('body-parser');
 app.use(bodyParse.json());
 
+//use of middleware function logRequest
+const logRequest=(req,res,next)=>{
+  console.log(`${new Date().toLocaleString()} Request made to : ${req.originalUrl}`);
+  next();//iska matlab h ki humne function bana diya middleware iske bad koi middleware func h to execute karo nhi to server vala response ka kam karo
+}
+app.use(logRequest);//ye sare routes pe lag rha h middleware 
+
+
+
+app.use(passport.initialize());
+const LocalAuthMiddleware=passport.authenticate('local',{session:false});
 app.get('/', function (req, res) {
   res.send('welcome to my hotel')
 })
 
 //import Person Router
 const personRoutes=require('./routes/personRoutes');
-app.use('/person',personRoutes);
+app.use('/person',LocalAuthMiddleware,personRoutes);
 
 //import menuItem Router
 const menuItemRoutes=require('./routes/menuItemRoutes');
